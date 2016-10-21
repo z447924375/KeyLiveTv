e530fb18b676962d323443825c008fc6506bef80
朱晓恒 <1037515398@qq.com>
2016-10-21 08:38:51 +0800

    baseFrag

--- app/src/main/java/com/keyliveapp/keylivetv/baseclass/BaseFragment.java
+++ app/src/main/java/com/keyliveapp/keylivetv/baseclass/BaseFragment.java
@@ -1,8 +1,41 @@
 package com.keyliveapp.keylivetv.baseclass;
 
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by dllo on 16/10/20.
  */
 
-public class BaseFragment {
+public abstract class BaseFragment extends Fragment {
+    protected Context mContext;
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.mContext = context;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(setLayout(), container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initView();
+    }
+
+    protected abstract void initView();
+
+    protected abstract int setLayout();
+
 }
