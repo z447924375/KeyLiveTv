2220c5ac97e782e67a4da07f567f31bf5354d3ca
朱晓恒 <1037515398@qq.com>
2016-10-22 11:11:58 +0800

    删除布局 zxh

--- app/src/main/java/com/keyliveapp/keylivetv/main/MainActivity.java
+++ app/src/main/java/com/keyliveapp/keylivetv/main/MainActivity.java
@@ -1,17 +1,23 @@
 package com.keyliveapp.keylivetv.main;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import com.keyliveapp.keylivetv.R;
+import com.keyliveapp.keylivetv.baseclass.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int setlayout() {
+        return R.layout.activity_main;
+    }
 
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void inidate() {
 
     }
 }
