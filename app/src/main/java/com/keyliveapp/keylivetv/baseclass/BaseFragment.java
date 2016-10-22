05a0aec33b93d8ff11afc9751cede8bc97184630
朱晓恒 <1037515398@qq.com>
2016-10-22 08:36:25 +0800

    修改基类   10.22

--- app/src/main/java/com/keyliveapp/keylivetv/baseclass/BaseFragment.java
+++ app/src/main/java/com/keyliveapp/keylivetv/baseclass/BaseFragment.java
@@ -1,9 +1,12 @@
 package com.keyliveapp.keylivetv.baseclass;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +17,7 @@ import android.view.ViewGroup;
 
 public abstract class BaseFragment extends Fragment {
     protected Context mContext;
-
+    protected FragmentManager mFragmentManager;
 
     @Override
     public void onAttach(Context context) {
@@ -22,20 +25,50 @@ public abstract class BaseFragment extends Fragment {
         this.mContext = context;
     }
 
+    protected abstract int setLayout();
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(setLayout(), container, false);
     }
 
+
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         initView();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initDate();
+    }
+
+    protected <T extends View> T getViewLayout(int id) {
+        return (T) getView().findViewById(id);
+    }
+
+    protected <T extends View> T getViewLayout(int id, View v) {
+        return (T) getView().findViewById(id);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    protected void changeFragmentTo(Fragment fragment, int framlayoutId) {
+        FragmentManager fragmentManager = getChildFragmentManager();
+        fragmentManager.beginTransaction().replace(framlayoutId, fragment).commit();
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    protected void removeFragment(Fragment fragment){
+        FragmentManager fragmentManager = getChildFragmentManager();
+        fragmentManager.beginTransaction().remove(fragment).commit();
     }
 
     protected abstract void initView();
 
-    protected abstract int setLayout();
-
+    protected abstract void initDate();
 }
