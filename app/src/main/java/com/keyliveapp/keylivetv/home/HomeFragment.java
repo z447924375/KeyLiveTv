cce61819225bb46b950d80567fb91559be0440e0
dllo <vambonne@163.com>
2016-10-27 14:20:06 +0800

    1026   2.20

--- app/src/main/java/com/keyliveapp/keylivetv/home/HomeFragment.java
+++ app/src/main/java/com/keyliveapp/keylivetv/home/HomeFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.keyliveapp.keylivetv.R;
 import com.keyliveapp.keylivetv.baseclass.BaseFragment;
 import com.keyliveapp.keylivetv.tools.HttpManager;
 import com.keyliveapp.keylivetv.tools.OnCompletedListener;
--- app/src/main/java/com/keyliveapp/keylivetv/main/MainActivity.java
+++ app/src/main/java/com/keyliveapp/keylivetv/main/MainActivity.java
@@ -2,32 +2,19 @@ package com.keyliveapp.keylivetv.main;
 
 
 import android.support.design.widget.TabLayout;
-
-
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 
 import android.support.v4.view.ViewPager;
-import android.view.View;
 
 import com.keyliveapp.keylivetv.R;
 import com.keyliveapp.keylivetv.baseclass.BaseActivity;
-<<<<<<< HEAD
-import com.keyliveapp.keylivetv.baseclass.BaseFragment;
 import com.keyliveapp.keylivetv.classify.ClassifyFragment;
-import com.keyliveapp.keylivetv.homepage.HomepageFragment;
 
 import java.util.ArrayList;
-=======
-import com.keyliveapp.keylivetv.classify.ClassifyFragment;
 import com.keyliveapp.keylivetv.discovery.DiscoveryFragment;
 import com.keyliveapp.keylivetv.home.HomeFragment;
 import com.keyliveapp.keylivetv.mine.MineFragment;
->>>>>>> 99a59c5407fc6a86875816e9f3159f297407facb
 
-import java.util.ArrayList;
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class MainActivity extends BaseActivity {
 //    private ViewPager lzViewPager;
