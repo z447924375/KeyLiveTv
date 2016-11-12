af2d52253967c0545c296582859e84dd97e40dbc
朱晓恒 <1037515398@qq.com>
2016-11-12 21:15:01 +0800

    g

--- app/src/main/java/com/keyliveapp/keylivetv/home/homeui/homeclickcallback/OnHomeHoriItemClickListner.java
+++
@@ -1,9 +0,0 @@
-package com.keyliveapp.keylivetv.home.homeui.homeclickcallback;
-
-/**
- * Created by dllo on 16/11/11.
- */
-
-public interface OnHomeHoriItemClickListner {
-    void horiItemClick(int position);
-}
