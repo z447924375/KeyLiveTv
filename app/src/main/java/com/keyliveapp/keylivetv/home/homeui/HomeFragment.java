eeacd76d18803c31658bfccc3c39f1f01b52fedd
侯明洋 <1010700586@qq.com>
2016-11-14 19:37:35 +0800

    esc

--- app/src/main/java/com/keyliveapp/keylivetv/home/homeui/HomeFragment.java
+++ app/src/main/java/com/keyliveapp/keylivetv/home/homeui/HomeFragment.java
@@ -187,12 +187,10 @@ public class HomeFragment extends BaseFragment implements IHomeView, View.OnClic
 
             @Override
             public void onPageSelected(int position) {
-<<<<<<< HEAD
                 bannerTitle.setText("   " + homeBean.getData().getBanner()
                         .get(Math.abs(position - 1) % homeBean.getData().getBanner().size()).getTitle() + "   ");
-=======
-                bannerTitle.setText(homeBean.getData().getBanner().get( Math.abs(position - 1)%homeBean.getData().getBanner().size()).getTitle());
->>>>>>> feature/Recycler下拉刷新
+
+
             }
 
             @Override
