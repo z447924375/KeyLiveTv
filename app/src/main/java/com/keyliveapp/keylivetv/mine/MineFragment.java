5a1bcff4f2dec8447d7314fe6586e63367b9fa75
朱晓恒 <1037515398@qq.com>
2016-11-15 17:17:32 +0800

    合

--- app/src/main/java/com/keyliveapp/keylivetv/mine/MineFragment.java
+++ app/src/main/java/com/keyliveapp/keylivetv/mine/MineFragment.java
@@ -2,32 +2,29 @@ package com.keyliveapp.keylivetv.mine;
 
 import android.content.Intent;
 import android.view.View;
-<<<<<<< HEAD
-=======
+
 import android.widget.ImageView;
->>>>>>> 24559ad6b34940606f7a6e139bf03d07d992eeb6
+
 import android.widget.RelativeLayout;
 
 import com.keyliveapp.keylivetv.R;
 import com.keyliveapp.keylivetv.baseclass.BaseFragment;
-<<<<<<< HEAD
+
 import com.keyliveapp.keylivetv.mine.like.LikeActivity;
-=======
 import com.keyliveapp.keylivetv.login.LoginActivity;
->>>>>>> 24559ad6b34940606f7a6e139bf03d07d992eeb6
 
 /**
  * Created by dllo on 16/10/22.
  */
 
 public class MineFragment extends BaseFragment implements View.OnClickListener {
-<<<<<<< HEAD
+
     private RelativeLayout mineSubLayout;
-=======
+
 
     private RelativeLayout login;
     private ImageView setting;
->>>>>>> 24559ad6b34940606f7a6e139bf03d07d992eeb6
+
 
     @Override
     protected int setLayout() {
@@ -36,43 +33,40 @@ public class MineFragment extends BaseFragment implements View.OnClickListener {
 
     @Override
     protected void initView() {
-<<<<<<< HEAD
+
         mineSubLayout = getViewLayout(R.id.iv_mine_sub_ll);
-=======
+
         login = getViewLayout(R.id.rv_mine_login);
         setting = getViewLayout(R.id.iv_login_setting);
->>>>>>> 24559ad6b34940606f7a6e139bf03d07d992eeb6
+
 
     }
 
     @Override
     protected void initDate() {
-<<<<<<< HEAD
         mineSubLayout.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        Intent intent = new Intent(getActivity(), LikeActivity.class);
-        startActivity(intent);
-=======
-        login.setOnClickListener(this);
-        setting.setOnClickListener(this);
-    }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.rv_mine_login :
+            case R.id.rv_mine_login:
                 Intent intent = new Intent(getActivity(), LoginActivity.class);
                 startActivity(intent);
                 break;
-            case R.id.iv_login_setting :
+            case R.id.iv_login_setting:
                 Intent intent1 = new Intent(getActivity(), SettingActivity.class);
                 startActivity(intent1);
                 break;
+            case R.id.iv_mine_sub_ll:
+                Intent intent2 = new Intent(getActivity(), LikeActivity.class);
+                startActivity(intent2);
+                login.setOnClickListener(this);
+                setting.setOnClickListener(this);
+
+                break;
 
         }
->>>>>>> 24559ad6b34940606f7a6e139bf03d07d992eeb6
     }
 }
